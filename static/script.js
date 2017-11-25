eefafc26c10255fd42657d080d83864f25bda839
Navaneeth Nagesh <ashi-rash@gmail.com>
2017-11-25 23:45:51 +0530

    initial commit

---
+++ static/script.js
@@ -0,0 +1,52 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+'use strict';
+
+$(document).ready(function () {
+    $('.contact-header').on('click', function (ev) {
+        $('.contact-container').slideToggle();
+    });
+
+});
+
+//building
+
+var property = $('.property');
+
+var toggleClass = function(el) {
+  el.toggleClass('build');
+};
+
+property.on('click', function() {
+  toggleClass(property);
+});
+
+setTimeout(function() { toggleClass(property) }, 250);
+
+// carousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    nav : true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+
+});
